5b3fd63a0958f331f78fcaa448f04644ebed8c23
MuhammadFawzii <imuhammadfawzi@gmail.com>
2025-03-10 09:23:43 +0200

    CategoryCommit

---
+++ ECommerceBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceBookWeb.Data;
+using ECommerceBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoriesList=_db.Categories.ToList();
+            return View(categoriesList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category) {
+            if (ModelState.IsValid) {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+
+        
+        }
+    }
+}
