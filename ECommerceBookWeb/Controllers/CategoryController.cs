2011e1512c9ae7375e05608081eb1cf0f7ec7174
MuhammadFawzii <imuhammadfawzi@gmail.com>
2025-03-12 05:49:09 +0200

    Section2Finished

--- ECommerceBookWeb/Controllers/CategoryController.cs
+++ ECommerceBookWeb/Controllers/CategoryController.cs
@@ -22,15 +22,89 @@ namespace ECommerceBookWeb.Controllers
         }
         [HttpPost]
         public IActionResult Create(Category category) {
+            //Custom Validation Message
+            if (category.Name==category.DisplayOrder.ToString()) {
+                ModelState.AddModelError("", "the Display Order can't exactly match the Name.");
+                //ModelState.AddModelError("Name", "the Display Order can't exactly match the Name.");
+
+            }
             if (ModelState.IsValid) {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"]= "Category Created successfully";
                 return RedirectToAction("Index");
             }
             return View();
             
 
         
+        }
+        public IActionResult Edit(int?id)
+        {
+            if (id == null||id==0) { 
+                return NotFound();
+           
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //Custom Validation Message
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "the Display Order can't exactly match the Name.");
+                //ModelState.AddModelError("Name", "the Display Order can't exactly match the Name.");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+
+            }
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int?id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
+
+            return RedirectToAction("Index");
+           
+
+
         }
     }
 }
