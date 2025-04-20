e6628b0b817ca031b1bfc0dff878e10ff57e0a00
MuhammadFawzii <imuhammadfawzi@gmail.com>
2025-04-20 06:10:15 +0200

    part1 orderpage

---
+++ ECommerceBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using ECommerceBook.DataAccess.Repository;
+using ECommerceBook.DataAccess.Repository.IRepository;
+using ECommerceBook.Models;
+using ECommerceBook.Models.ViewModels;
+using ECommerceBook.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace ECommerceBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public OrderController(IUnitOfWork _unitOfWork)
+        {
+            this.unitOfWork = _unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int orderHeaderId) {
+            OrderVM orderVM = new OrderVM()
+            {
+                OrderHeader=unitOfWork.OrderHeaderRepository.Get(u => u.Id==orderHeaderId, includeProperties: "ApplicationUser"),
+                OrderDetails=unitOfWork.OrderDetailRepository.GetAll(u => u.OrderHeaderId==orderHeaderId, includeProperties: "Product")
+            };
+            return View(orderVM);
+        }
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaderList = unitOfWork.OrderHeaderRepository.GetAll(includeProperties: "ApplicationUser")
+                
+                .ToList();
+            switch (status)
+            {
+                case "pending":
+                    orderHeaderList =orderHeaderList.Where(u => u.PaymentStatus==SD.PaymentStatusPending);
+                    break;
+                case "inprocess":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaderList = orderHeaderList.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+    }
+}
---
+++ ECommerceBookWeb/wwwroot/js/order.js
@@ -0,0 +1,29 @@
+﻿var dataTable;
+$(document).ready(function () {
+    var status = window.location.search.split('=')[1];
+    loadDataTable(status);
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: '/admin/order/getall?status='+status,
+            dataSrc: 'data'
+        },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "10%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "8%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-100 btn-group" role="group" >
+                                 <a href="/admin/order/details?orderHeaderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>
+                            </div>`;
+                }, "width": "10%"
+            }
+        ]
+    }); }
